ddde91ca251ba3133e367792f21f4aa0c772d9b7
eves <evesjapan@gmail.com>
2018-04-20 23:28:39 +0900

    리엑트 새파일 올림.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./src/App";
 import typography from "./typography";
+import _ from "lodash";
 
 ReactDOM.render(<App />, document.getElementById("app"));
